7b2dfbbac9b652c0c01c618e7d206534bafc001b
Lucyus269 <145916453+Lucyus269@users.noreply.github.com>
2024-05-06 09:34:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form-lista');
+const NomeTarefa = [];
+let linhas = '';
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    AddTarefa();
+    atualizaTabela();
+    
+})
+
+function AddTarefa() {
+    const InputTarefa = document.getElementById('Tarefa');
+
+    if(NomeTarefa.includes(InputTarefa.value)){
+        alert(`a tarefa ${InputTarefa.value} já foi inserida!`);
+    }else {
+    
+    NomeTarefa.push(InputTarefa.value);
+
+    let linha = `<ul>`;
+    linha += `<li> ${InputTarefa.value}`;
+    linha += `</ul>`;
+
+    linhas += linha;
+    }
+    
+    InputTarefa.value = '';
+}
+
+function atualizaTabela(){
+    const Tabelaraiz = document.querySelector('ul');
+    Tabelaraiz.innerHTML = linhas;
+}
